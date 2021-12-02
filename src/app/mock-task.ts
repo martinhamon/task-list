c2be89c5109b542d3f1fce242af349ebeb271551
MH <martin@m-code.com.ar>
2021-12-02 18:06:08 -0300

    lista tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+  {
+    id:1,
+    text: "Terminar modulo uno ",
+    day: " Agosto 5 a las 12:00",
+    reminder: true
+  },
+  {
+    id:2,
+    text: "Terminar modulo dos ",
+    day: " Agosto 5 a las 17:00",
+    reminder: true
+  },
+  {
+    id:3,
+    text: "Terminar modulo tres ",
+    day: " Agosto 5 a las 19:00",
+    reminder: false
+  },
+  {
+    id:4,
+    text: "Terminar modulo cuatro ",
+    day: " Agosto 5 a las 12:00",
+    reminder: false
+  }
+
+]
